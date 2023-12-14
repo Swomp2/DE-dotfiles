ca9819c9acc7fd240092e8ce0f069c89c33bf121
Swomp2 <swomp226@gmail.com>
2023-12-14 22:58:57 +0300

    some small gentoo specific changes:)

--- .config/bemenu/power.fish
+++ .config/bemenu/power.fish
@@ -3,9 +3,9 @@
 set answer (echo "Выключиться"\n"Перезагрузиться"\n"Выйти"| bemenu --nb "#282828" --nf "#fbf1c7" --tb "#98971a" --tf "#282828" --fb "#282828" --ff "#fbf1c7" --hb "#98971a" --hf "#282828" --ab "#282828" --af "#fbf1c7" -H 10 -M 1130 -c --border 2 --bdr "#98971a" --prompt="Что сделать? " --list 5 -i --fn "Ubuntu Regular 10")
 
 if test $answer = "Перезагрузиться"
-   exec systemctl reboot
+   exec loginctl reboot
 else if test $answer = "Выключиться"
-   exec systemctl poweroff
+   exec loginctl poweroff
 else if test $answer = "Выйти"
    exec hyprctl dispatch exit
 end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,16 +3,9 @@ if status is-login
 	export PATH="$PATH:/usr/local/libexec"
 	export PATH="$PATH:/home/swomp/.local/bin"
 	export PATH="$PATH:/home/swomp/.cargo/bin"
-	export QT_QPA_PLATFORMTHEME="qt5ct"
-	export VDPAU_DRIVER="radeonsi"
-	export HYPRLAND_CMD=Hyprland
-	export XDG_SESSION_TYPE=wayland
-	export XDG_CURRENT_DESKTOP=Hyprland
-	export HYPRSHOT_DIR="/home/swomp/Pictures/screenshots"
-	exec dbus-run-session Hyprland	
 end
 
-alias hx="helix"
+export VISUAL=hx
 
 function fish_prompt
 	 echo (set_color normal)'['(set_color "#b8bb26")$USER'|'$hostname(set_color normal)']-'(set_color normal)'['(set_color "#fe8019")(pwd)(set_color normal)']-'(set_color normal)"["(set_color "#fabd2f")(date +"%a %D %I:%M")(set_color normal)"]"(set_color "#83a598")'==>' (set_color normal)
